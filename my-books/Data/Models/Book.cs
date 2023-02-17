4b1ea05b5148555c1d4e1ae45b1514630717aea2
Сергей Наумов <sergey_n@holdingbp.ru>
2023-02-17 18:04:59 +0300

    feat: add book model

---
+++ my-books/Data/Models/Book.cs
@@ -0,0 +1,25 @@
+﻿namespace my_books.Data.Models
+{
+    public class Book
+    {
+        public int Id { get; set; }
+
+        public string Title { get; set; } = string.Empty;
+
+        public string Description { get; set; } = string.Empty;
+
+        public bool IsRead { get; set; }
+
+        public DateTime? DateRead { get; set; }
+
+        public int? Rate { get; set; }
+
+        public string Genre { get; set; } = string.Empty;
+
+        public string Author { get; set; } = string.Empty;
+
+        public string CoverUrl { get; set; } = string.Empty;
+
+        public DateTime? DateAdded { get; set; }
+    }
+}
