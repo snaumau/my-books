9f778dff54399e49f51daf332359c8bbccb51dbc
Сергей Наумов <sergey_n@holdingbp.ru>
2023-02-19 21:50:48 +0300

    feat: add seed data

---
+++ my-books/Data/AppDbInitializer.cs
@@ -0,0 +1,43 @@
+﻿using my_books.Data.Models;
+
+namespace my_books.Data
+{
+    public class AppDbInitializer
+    {
+        public static void Seed(IApplicationBuilder applicationBuilder)
+        {
+            using (var serviceScope = applicationBuilder.ApplicationServices.CreateScope())
+            {
+                var context = serviceScope.ServiceProvider.GetService<AppDbContext>();
+
+                if (!context!.Books.Any())
+                {
+                    context.Books.AddRange(new Book
+                    {
+                        Title = "1st Book Tittle",
+                        Description = "1st book Description",
+                        IsRead = true,
+                        DateRead = DateTime.Now.AddDays(-10),
+                        Rate = 4,
+                        Genre = "Biography",
+                        Author = "First Author",
+                        CoverUrl = "https....",
+                        DateAdded = DateTime.Now
+                    },
+                    new Book
+                    {
+                        Title = "2nd Book Tittle",
+                        Description = "2nd book Description",
+                        IsRead = false,
+                        Genre = "Biography",
+                        Author = "Second Author",
+                        CoverUrl = "https....",
+                        DateAdded = DateTime.Now
+                    });
+
+                    context.SaveChanges();
+                }
+            }
+        }
+    }
+}
--- my-books/Program.cs
+++ my-books/Program.cs
@@ -28,4 +28,6 @@ app.UseAuthorization();
 
 app.MapControllers();
 
+AppDbInitializer.Seed(app);
+
 app.Run();
